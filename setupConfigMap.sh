85ee102513e14b384e90f3562dd74eded7c0a30f
Joseph Yang <yang_joseph@ca.ibm.com>
2022-01-10 12:25:45 -0500

    init

---
+++ setupConfigMap.sh
@@ -0,0 +1,15 @@
+export SECRET_PATH=mystore/env1
+
+oc apply -f - << _EOF_
+apiVersion: v1
+data:
+  secrets: |
+    ns1/my-secret1
+    ns1/my-secret2
+    ns1/my-secret3
+    ns1/my-secret4
+  gopass-path: $SECRET_PATH
+kind: ConfigMap
+metadata:
+  name: gopass-secrets
+_EOF_
